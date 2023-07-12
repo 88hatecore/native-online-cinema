bcc5f1df4bb3afa526fecbddbd555285a6c798d0
Ilya Dolgov <prhatecore@gmail.com>
2023-07-12 11:47:05 +0300

    add prettier, add babel, add tsconfig

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Header from "@/components/Header";
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, View} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header />
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -13,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#333',
     alignItems: 'center',
     justifyContent: 'center',
   },
